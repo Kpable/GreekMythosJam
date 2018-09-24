44ddf72f9da45a6b8b30dc0a19ec7530c0dd1f4d
lestherreynoso <lhreynos@gmail.com>
2018-09-24 19:58:47 -0400

    Got some movement going

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TopDownMovement : Movement {
+
+    public float speed = 15f; 
+
+    public override void Move(PlayerControl movableObject)
+    {
+        Rigidbody2D body = movableObject.GetComponent<Rigidbody2D>();
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(name + ": speed: " + speed);
+
+        Vector2 movement = movableObject.transform.up * vAxis * speed * Time.deltaTime;
+        movement += Vector2.right * hAxis * speed * Time.deltaTime;
+        //Debug.Log(name + ": vAxis: " + vAxis);
+
+        //Debug.Log(name + ": movement: " + movement);
+        body.MovePosition(body.position + movement);
+
+        Turn();
+    }
+
+    public void Turn()
+    {
+
+    }
+
+}
