2752204ecda8a1836076b2b6a51ed526b8835981
lestherreynoso <lhreynos@gmail.com>
2018-09-24 20:45:08 -0400

    playing around with some additional movement/rotation types

---
+++ Assets/Scripts/Movement/ThrustMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ThrustMovement : Movement {
+
+    [HeaderAttribute("Movement Variables")]
+    public float thrust = 25f;
+    public Rotation rotation;
+
+    private Rigidbody2D body;
+
+    public override void Move(PlayerControl movableObject)
+    {
+        body = movableObject.GetComponent<Rigidbody2D>();
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(0, vAxis * thrust * Time.deltaTime, 0);
+
+        body.AddForce(body.transform.rotation * force);
+
+        rotation.Turn(movableObject);
+    }
+
+}
