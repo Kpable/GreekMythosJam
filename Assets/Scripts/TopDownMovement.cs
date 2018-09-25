ae1d3cf61bf8811ef10ae2d5ea504829457d3801
lestherreynoso <lhreynos@gmail.com>
2018-09-24 20:15:32 -0400

    Look at mouse rotation

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -5,29 +5,25 @@ using UnityEngine;
 [CreateAssetMenu]
 public class TopDownMovement : Movement {
 
-    public float speed = 15f; 
+    [HeaderAttribute("Movement Variables")]
+    public float speed = 5f;
+    public Rotation rotation;
+
+    private Rigidbody2D body; 
 
     public override void Move(PlayerControl movableObject)
     {
-        Rigidbody2D body = movableObject.GetComponent<Rigidbody2D>();
+        body = movableObject.GetComponent<Rigidbody2D>();
         float hAxis = Input.GetAxisRaw("Horizontal");
         float vAxis = Input.GetAxisRaw("Vertical");
 
-        Debug.Log(name + ": speed: " + speed);
-
-        Vector2 movement = movableObject.transform.up * vAxis * speed * Time.deltaTime;
+        Vector2 movement = Vector2.up* vAxis * speed * Time.deltaTime;
         movement += Vector2.right * hAxis * speed * Time.deltaTime;
         //Debug.Log(name + ": vAxis: " + vAxis);
 
         //Debug.Log(name + ": movement: " + movement);
         body.MovePosition(body.position + movement);
 
-        Turn();
+        rotation.Turn(movableObject);
     }
-
-    public void Turn()
-    {
-
-    }
-
 }
