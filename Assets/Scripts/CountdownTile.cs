7a0c67932d2a656c65094930df683344a7a10a3f
lestherreynoso <lhreynos@gmail.com>
2018-10-09 20:04:09 -0400

    beginnings of countdown

---
+++ Assets/Scripts/CountdownTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CountdownTile : MonoBehaviour {
+
+    public Vector3Int Wall1TilePosition, Wall2TilePosition;
+    public Tilemap WallTileMap;
+
+	// Use this for initialization
+	void Start () {
+        GameObject wall1Tile = WallTileMap.GetInstantiatedObject(Wall1TilePosition);
+        GameObject wall2Tile = WallTileMap.GetInstantiatedObject(Wall2TilePosition);
+
+        wall2Tile.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
