97e976a275561adc7b7212fe5049ed91f79d078c
jaredgav <jaredgav@buffalo.edu>
2018-10-10 19:11:43 -0400

    Test UI elements

---
+++ Assets/movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public Rigidbody2D body;
+    public int speed;
+    public float drag;
+
+	// Use this for initialization
+	void Start () {
+        drag = 3f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        body.drag = drag;
+        if (Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(Vector2.down * speed);
+            body.MoveRotation(-90);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(Vector2.up * speed);
+            body.MoveRotation(90);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            body.AddForce(Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            body.AddForce(Vector2.left * speed);
+        }
+    }
+}
