d9587d90230d03a646e0cbd2d2592c8ff1d3332f
lestherreynoso <lhreynos@gmail.com>
2018-10-10 19:44:02 -0400

    working on adding trigger response

--- Assets/Scripts/CountdownTile.cs
+++ Assets/Scripts/CountdownTile.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class CountdownTile : MonoBehaviour {
+[CreateAssetMenu]
+public class CountdownTile : TileBase {
 
-    public Vector3Int Wall1TilePosition, Wall2TilePosition;
-    public Tilemap WallTileMap;
+    public Vector3Int AlternatePosition;
+    public Sprite sprite;
 
-	// Use this for initialization
-	void Start () {
-        GameObject wall1Tile = WallTileMap.GetInstantiatedObject(Wall1TilePosition);
-        GameObject wall2Tile = WallTileMap.GetInstantiatedObject(Wall2TilePosition);
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        Debug.Log("GetTileData");
+        tileData.sprite = sprite;
+        tileData.colliderType = Tile.ColliderType.Sprite;
+        base.GetTileData(position, tilemap, ref tileData);
+    }
 
-        wall2Tile.SetActive(false);
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        Debug.Log("StartUp");
+        Debug.Log("GameObject is " + go == null);
+        if (go)
+        {
+            go.AddComponent<OnTriggerEnter2DEvent>().OnEvent += OnTriggerEnter2D;
+        }
+
+        return true;
+    }
+
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        Debug.Log("RefreshTile");
+        base.RefreshTile(position, tilemap);
+    }
+
+    // Use this for initialization
+    void Start () {
+        
     }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void OnTriggerEnter2D (Collider2D collision)
+    {
+        Debug.Log("Triggered by: " + collision.name);
+    }
 }
---
+++ Assets/Scripts/OnTriggerEnter2DEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OnTriggerEnter2DEvent : MonoBehaviour {
+
+    public delegate void TriggerEnter2DEvent(Collider2D collision);
+    public event TriggerEnter2DEvent OnEvent;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (OnEvent != null) OnEvent(collision);
+        Debug.Log("OnTriggerEnter2DEvent:Triggered by: " + collision.name);
+
+    }
+}
