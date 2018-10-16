d45b37aff922ca3a148312ffbfe3ee309cc5dfe7
jaredgav <jaredgav@buffalo.edu>
2018-10-15 22:10:37 -0400

    Added Fog of war mechanic

---
+++ Assets/Scenes/Jared/FogMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogMovement : MonoBehaviour {
+
+    public GameObject LeftBound, RightBound, LowBound, HighBound;
+    private Vector2 leftBound, rightBound, lowBound, highBound;
+    public Rigidbody2D body;
+    private float speed;
+    private Vector2 direction;
+    private int frames;
+
+    // Use this for initialization
+    void Start () {
+        speed = .3f;
+        frames = 20;
+        body.position = new Vector2(Random.Range(leftBound.x, rightBound.x), 
+                                    Random.Range(leftBound.y, rightBound.y));
+        if (Random.value > .5f)
+            direction = Vector2.up;
+        else
+            direction = Vector2.down;
+ 	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // Random360();
+        OscillateUpDown();
+	}
+
+    void OscillateUpDown()
+    {
+        body.velocity = speed * direction;
+
+        frames--;
+        if (frames <= 1)
+        {
+            if (direction == Vector2.up)
+                direction = Vector2.down;
+            else
+                direction = Vector2.up;
+            frames = 100;
+        }
+    }
+
+    void Random360()
+    {
+        Vector2 direction = new Vector2();
+        switch (Random.Range(0, 2))
+        {
+            case 0:
+                direction = Vector2.up;
+                break;
+            case 1:
+                direction = Vector2.down;
+                break;
+            case 2:
+                direction = Vector2.left;
+                break;
+            case 3:
+                direction = Vector2.right;
+                break;
+        }
+
+        body.velocity = speed * direction;
+        speed = Random.Range(1, 4);
+    }
+}
---
+++ Assets/Scenes/Jared/FogSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogSpawn : MonoBehaviour {
+
+    public GameObject cloudPrefab;
+    
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 30; i++)
+        {
+            var cloud = (GameObject) Instantiate(
+                cloudPrefab,
+                transform.position * Random.insideUnitCircle,
+                transform.rotation
+            );
+
+            cloud.GetComponent<SpriteRenderer>().color = new Color(255,255,255,Random.value+.5f);
+            cloud.GetComponent<DistanceJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+            cloud.GetComponent<DistanceJoint2D>().distance = 3 * Random.value;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
