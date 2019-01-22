b1b338b3b199141b05408352e8424a33f5dfa2a0
jaredgav <jaredgav@buffalo.edu>
2019-01-21 21:53:17 -0500

    Updated cloud sspawn and layers

--- Assets/Scenes/Jared/Populator.cs
+++ Assets/Scenes/Jared/Populator.cs
@@ -5,27 +5,49 @@ using UnityEngine;
 public class Populator : MonoBehaviour {
 
     public GameObject fogSpawnerPrefab;
-    public GameObject LeftBound, RightBound, LowBound, HighBound;
-    private Vector2 leftBound, rightBound, lowBound, highBound;
-    public int fogSpawnerCount;
+    public float leftBound, rightBound, lowBound, highBound;
+    public int fogSpawnerCount; 
+    public Camera mainCamera;
+    public BoxCollider2D safeHaven;
 
     // Use this for initialization
     void Start () {
-        leftBound = LeftBound.GetComponent<Transform>().position;
-        rightBound = RightBound.GetComponent<Transform>().position;
-        lowBound = LowBound.GetComponent<Transform>().position;
-        highBound = HighBound.GetComponent<Transform>().position;
 
         for (int i = 0; i < fogSpawnerCount; i++)
         {
+            Vector3 bounds = GetSpawnBounds();
+            bounds.x = Random.Range(leftBound, rightBound);
+            bounds.y = Random.Range(highBound, lowBound);
+
             var fogSpawner = (GameObject) Instantiate(
                 fogSpawnerPrefab,
-                new Vector3(Random.Range(Camera.main.ScreenToViewportPoint(leftBound).x, Camera.main.ScreenToViewportPoint(rightBound).x),
-                            Random.Range(Camera.main.ScreenToViewportPoint(lowBound).y, Camera.main.ScreenToViewportPoint(highBound).y)),
+                bounds,
                 transform.rotation
             );
         }
 	}
+
+    Vector3 GetSpawnBounds()
+    {
+        Vector3 finalBounds = new Vector3();
+
+        finalBounds.x = Random.Range(leftBound, rightBound);
+        finalBounds.y = Random.Range(highBound, lowBound);
+
+        while(finalBounds.x <= (safeHaven.transform.position.x + safeHaven.size.x/2) &&
+            finalBounds.x >= (safeHaven.transform.position.x - safeHaven.size.x / 2))
+        {
+            finalBounds.x = Random.Range(leftBound, rightBound);
+        }
+
+        while (finalBounds.y <= (safeHaven.transform.position.y + safeHaven.size.y / 2) &&
+           finalBounds.y >= (safeHaven.transform.position.y - safeHaven.size.y / 2))
+        {
+            finalBounds.y = Random.Range(highBound, lowBound);
+        }
+
+        return finalBounds;
+    }
 	
 	// Update is called once per frame
 	void Update () {
